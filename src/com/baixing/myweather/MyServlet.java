29c01cdc3c19c8d83c2f56d4b6b54b267e90acc1
wdchang <13changice@tongji.edu.cn>
2016-07-12 11:10:53 +0800

    init commit

---
+++ src/com/baixing/myweather/MyServlet.java
@@ -0,0 +1,70 @@
+package com.baixing.myweather;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * Servlet implementation class MyServlet
+ */
+@WebServlet("/MyServlet")
+public class MyServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public MyServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		StringBuilder sb = new StringBuilder();
+		BufferedReader br = request.getReader();
+		String str = null;
+		while ((str = br.readLine()) != null) {
+			sb.append(str);
+		}
+		
+		System.out.println("name -> "+str.toString());
+		
+		/*JSONObject jObj;
+		String name = null;
+		try {
+			jObj = new JSONObject(sb.toString());
+			name = jObj.getString("name");
+		} catch (JSONException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		
+
+		response.setContentType("text/plain");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write("hello from java. you entered : " + str.toString());
+	}
+
+}
